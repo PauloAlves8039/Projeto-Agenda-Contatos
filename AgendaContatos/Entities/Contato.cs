3b78eb770e3c401f88bc9528fa563492cc6cf492
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-30 16:37:48 -0300

    Atualizando o método ToString

--- AgendaContatos/Entities/Contato.cs
+++ AgendaContatos/Entities/Contato.cs
@@ -23,7 +23,7 @@ namespace AgendaContatos.Entities
 
         public override string ToString()
         {
-            return string.Format("{0} = {1}, {2}", Nome, Email, Telefone);
+            return string.Format("{0} - {1}, {2}", Nome, Email, Telefone);
         }
     }
 }
