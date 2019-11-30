1da7882df6b0b8d2fe0a1da4c5857dd62a8eec6b
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-30 16:27:00 -0300

    Adicionando ações para os botões do formulário

--- AgendaContatos/frmAgendaContatos.Designer.cs
+++ AgendaContatos/frmAgendaContatos.Designer.cs
@@ -30,15 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAgendaContatos));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.pcbInformacao = new System.Windows.Forms.PictureBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.btnAdicionar = new System.Windows.Forms.ToolStripButton();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAdicionar = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.btnAtualizar = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
@@ -48,19 +48,19 @@
             this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
             this.btnLimpar = new System.Windows.Forms.ToolStripButton();
             this.btnSair = new System.Windows.Forms.ToolStripButton();
-            this.pcbInformacao = new System.Windows.Forms.PictureBox();
-            this.lbxContatos = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtNome = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.mktTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbxContatos = new System.Windows.Forms.ListBox();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
-            this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pcbInformacao)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -74,41 +74,14 @@
             this.panel1.Size = new System.Drawing.Size(634, 120);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // pcbInformacao
             // 
-            this.panel2.Controls.Add(this.mktTelefone);
-            this.panel2.Controls.Add(this.txtEmail);
-            this.panel2.Controls.Add(this.txtNome);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.lbxContatos);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(634, 241);
-            this.panel2.TabIndex = 1;
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnAdicionar,
-            this.toolStripSeparator1,
-            this.btnAtualizar,
-            this.toolStripSeparator2,
-            this.btnExcluir,
-            this.toolStripSeparator3,
-            this.btnSalvar,
-            this.toolStripSeparator4,
-            this.btnLimpar,
-            this.btnSair});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(634, 39);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
+            this.pcbInformacao.Image = ((System.Drawing.Image)(resources.GetObject("pcbInformacao.Image")));
+            this.pcbInformacao.Location = new System.Drawing.Point(289, 42);
+            this.pcbInformacao.Name = "pcbInformacao";
+            this.pcbInformacao.Size = new System.Drawing.Size(48, 49);
+            this.pcbInformacao.TabIndex = 2;
+            this.pcbInformacao.TabStop = false;
             // 
             // statusStrip1
             // 
@@ -124,15 +97,6 @@
             this.statusStrip1.Size = new System.Drawing.Size(634, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
-            // 
-            // btnAdicionar
-            // 
-            this.btnAdicionar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnAdicionar.Image = ((System.Drawing.Image)(resources.GetObject("btnAdicionar.Image")));
-            this.btnAdicionar.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnAdicionar.Name = "btnAdicionar";
-            this.btnAdicionar.Size = new System.Drawing.Size(36, 36);
-            this.btnAdicionar.Text = "Adicionar";
             // 
             // toolStripStatusLabel1
             // 
@@ -169,6 +133,37 @@
             this.toolStripStatusLabel5.Size = new System.Drawing.Size(127, 17);
             this.toolStripStatusLabel5.Text = "toolStripStatusLabel5";
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdicionar,
+            this.toolStripSeparator1,
+            this.btnAtualizar,
+            this.toolStripSeparator2,
+            this.btnExcluir,
+            this.toolStripSeparator3,
+            this.btnSalvar,
+            this.toolStripSeparator4,
+            this.btnLimpar,
+            this.btnSair});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(634, 39);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAdicionar.Image = ((System.Drawing.Image)(resources.GetObject("btnAdicionar.Image")));
+            this.btnAdicionar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(36, 36);
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
@@ -182,6 +177,7 @@
             this.btnAtualizar.Name = "btnAtualizar";
             this.btnAtualizar.Size = new System.Drawing.Size(36, 36);
             this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
             // 
             // toolStripSeparator2
             // 
@@ -196,6 +192,7 @@
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(36, 36);
             this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
             // toolStripSeparator3
             // 
@@ -210,6 +207,7 @@
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(36, 36);
             this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // toolStripSeparator4
             // 
@@ -224,6 +222,7 @@
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(36, 36);
             this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // btnSair
             // 
@@ -234,44 +233,49 @@
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(36, 36);
             this.btnSair.Text = "Sair";
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
-            // pcbInformacao
+            // panel2
             // 
-            this.pcbInformacao.Image = ((System.Drawing.Image)(resources.GetObject("pcbInformacao.Image")));
-            this.pcbInformacao.Location = new System.Drawing.Point(289, 42);
-            this.pcbInformacao.Name = "pcbInformacao";
-            this.pcbInformacao.Size = new System.Drawing.Size(48, 49);
-            this.pcbInformacao.TabIndex = 2;
-            this.pcbInformacao.TabStop = false;
+            this.panel2.Controls.Add(this.mktTelefone);
+            this.panel2.Controls.Add(this.txtEmail);
+            this.panel2.Controls.Add(this.txtNome);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lbxContatos);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(634, 241);
+            this.panel2.TabIndex = 1;
             // 
-            // lbxContatos
+            // mktTelefone
             // 
-            this.lbxContatos.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbxContatos.FormattingEnabled = true;
-            this.lbxContatos.Location = new System.Drawing.Point(13, 13);
-            this.lbxContatos.Name = "lbxContatos";
-            this.lbxContatos.Size = new System.Drawing.Size(250, 199);
-            this.lbxContatos.TabIndex = 0;
+            this.mktTelefone.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mktTelefone.Location = new System.Drawing.Point(349, 71);
+            this.mktTelefone.Mask = "(##)#####-####";
+            this.mktTelefone.Name = "mktTelefone";
+            this.mktTelefone.Size = new System.Drawing.Size(81, 23);
+            this.mktTelefone.TabIndex = 6;
             // 
-            // label1
+            // txtEmail
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(278, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nome:";
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(349, 42);
+            this.txtEmail.MaxLength = 40;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(250, 23);
+            this.txtEmail.TabIndex = 5;
             // 
-            // label2
+            // txtNome
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(278, 49);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Email:";
+            this.txtNome.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(349, 13);
+            this.txtNome.MaxLength = 40;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(250, 23);
+            this.txtNome.TabIndex = 4;
             // 
             // label3
             // 
@@ -283,32 +287,34 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Telefone:";
             // 
-            // txtNome
+            // label2
             // 
-            this.txtNome.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNome.Location = new System.Drawing.Point(349, 13);
-            this.txtNome.MaxLength = 40;
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(250, 23);
-            this.txtNome.TabIndex = 4;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email:";
             // 
-            // txtEmail
+            // label1
             // 
-            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEmail.Location = new System.Drawing.Point(349, 42);
-            this.txtEmail.MaxLength = 40;
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(250, 23);
-            this.txtEmail.TabIndex = 5;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(278, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
             // 
-            // mktTelefone
+            // lbxContatos
             // 
-            this.mktTelefone.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mktTelefone.Location = new System.Drawing.Point(349, 71);
-            this.mktTelefone.Mask = "(##)#####-####";
-            this.mktTelefone.Name = "mktTelefone";
-            this.mktTelefone.Size = new System.Drawing.Size(81, 23);
-            this.mktTelefone.TabIndex = 6;
+            this.lbxContatos.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxContatos.FormattingEnabled = true;
+            this.lbxContatos.Location = new System.Drawing.Point(13, 13);
+            this.lbxContatos.Name = "lbxContatos";
+            this.lbxContatos.Size = new System.Drawing.Size(250, 199);
+            this.lbxContatos.TabIndex = 0;
             // 
             // frmAgendaContatos
             // 
@@ -324,15 +330,16 @@
             this.Name = "frmAgendaContatos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agenda de Contatos";
+            this.Shown += new System.EventHandler(this.frmAgendaContatos_Shown);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbInformacao)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbInformacao)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- AgendaContatos/frmAgendaContatos.cs
+++ AgendaContatos/frmAgendaContatos.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AgendaContatos.Entities;
+using AgendaContatos.Utils;
+using AgendaContatos.Utils.Enum;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,132 @@ namespace AgendaContatos
 {
     public partial class frmAgendaContatos : Form
     {
+        private OperacaoEnum acao;
+
         public frmAgendaContatos()
         {
             InitializeComponent();
+        }
+
+        private void frmAgendaContatos_Shown(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(false);
+            AlterarBotoesAdicionarAtualizarExcluir(true);
+            AlterarEstadoCampos(false);
+            CarregarListaContatos();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(true);
+            AlterarBotoesAdicionarAtualizarExcluir(false);
+            AlterarEstadoCampos(true);
+            acao = OperacaoEnum.INCLUIR;
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(true);
+            AlterarBotoesAdicionarAtualizarExcluir(false);
+            AlterarEstadoCampos(true);
+            acao = OperacaoEnum.ALTERAR;
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Tem ceretza?", "Pergunta", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                int indiceExcluido = lbxContatos.SelectedIndex;
+                lbxContatos.SelectedIndex = 0;
+                lbxContatos.Items.RemoveAt(indiceExcluido);
+                List<Contato> contatoList = new List<Contato>();
+                foreach (Contato contato in lbxContatos.Items)
+                {
+                    contatoList.Add(contato);
+                }
+                ManipuladorArquivos.EscreverArquivo(contatoList);
+                CarregarListaContatos();
+                LimparCampos();
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Contato contato = new Contato
+            {
+                Nome = txtNome.Text,
+                Email = txtEmail.Text,
+                Telefone = mktTelefone.Text
+            };
+            List<Contato> contatosList = new List<Contato>();
+            foreach (Contato c in lbxContatos.Items)
+            {
+                contatosList.Add(c);
+            }
+
+            if (acao == OperacaoEnum.INCLUIR)
+            {
+                contatosList.Add(contato);
+            }
+            else
+            {
+                int indice = lbxContatos.SelectedIndex;
+                contatosList.RemoveAt(indice);
+                contatosList.Insert(indice, contato);
+            }
+
+            ManipuladorArquivos.EscreverArquivo(contatosList);
+            CarregarListaContatos();
+            AlterarBotoesSalvarECancelar(false);
+            AlterarBotoesAdicionarAtualizarExcluir(true);
+            LimparCampos();
+            AlterarEstadoCampos(false);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            AlterarBotoesSalvarECancelar(false);
+            AlterarBotoesAdicionarAtualizarExcluir(true);
+            AlterarEstadoCampos(false);
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AlterarEstadoCampos(bool estado)
+        {
+            txtNome.Enabled = estado;
+            txtEmail.Enabled = estado;
+            mktTelefone.Enabled = estado;
+        }
+
+        private void AlterarBotoesSalvarECancelar(bool estado)
+        {
+            btnSalvar.Enabled = estado;
+            btnLimpar.Enabled = estado;
+        }
+
+        private void AlterarBotoesAdicionarAtualizarExcluir(bool estado)
+        {
+            btnAdicionar.Enabled = estado;
+            btnAtualizar.Enabled = estado;
+            btnExcluir.Enabled = estado;
+        }
+
+        private void CarregarListaContatos()
+        {
+            lbxContatos.Items.Clear();
+            lbxContatos.Items.AddRange(ManipuladorArquivos.LerArquivo().ToArray());
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Clear();
+            txtEmail.Clear();
+            mktTelefone.Clear();
         }
     }
 }
