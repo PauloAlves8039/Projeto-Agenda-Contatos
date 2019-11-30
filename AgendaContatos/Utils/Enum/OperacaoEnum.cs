f1b302fe3bfb6414d27f640aeeec4d8ead82e98e
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-30 14:00:19 -0300

    Adicionando o enum OperacaoEnum

---
+++ AgendaContatos/Utils/Enum/OperacaoEnum.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Arquivo: enum OperacaoEnum
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter constantes referentes a inclusão e alteração de registros no formulário
+ * Data: 30/11/2019
+*/
+
+namespace AgendaContatos.Utils.Enum
+{
+    public enum OperacaoEnum
+    {
+        INCLUIR,
+        ALTERAR
+    }
+}
